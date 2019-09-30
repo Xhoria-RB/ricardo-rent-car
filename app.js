1b174d5b87db8072b6c17130b9b816ee3d8debe5
Ricardo <shirotokuro3715@gmail.com>
2019-09-30 00:10:34 -0400

    Deleting build test

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('./server/config/config');
 
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const routes = require('./server/routes/router');
 const apiRoutes = require('./server/routes/apiRoutes');
 
@@ -13,14 +12,9 @@ app.use(express.json());
 app.use('/', routes);
 app.use('/api', apiRoutes);
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', (_, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
-// app.use('*', (_, res) => res.json({
-//   message: 'Hello world'
-// }));
+app.use('*', (_, res) => res.json({
+  message: 'Hello world'
+}));
 
 
 module.exports = app;
